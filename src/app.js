37dcdb1ad4597be1773d0bcbd564d9140d23ed83
Satish Maurya <satish.maurya@oracle.com>
2017-01-14 19:39:59 +0530

    Revert "support for multi rendering"
    This reverts commit 5157d97b1c9dae30d22a5d3021c3fd6b8402b870.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,4 @@ import YahooWeather from 'src/YahooWeather';
 import './app.scss';
 
 const app = new YahooWeather();
-app.render(document.getElementById('widget-container-1'), 'mclean, va');
-app.render(document.getElementById('widget-container-2'), 'fairfax, va');
-app.render(document.getElementById('widget-container-3'), 'reston, va');
+app.render(document.getElementById('widget-container'));
---
+++ src/util/xhr.js
@@ -0,0 +1,3 @@
+import xhrPromise from 'xhr-promise';
+const xhr = new xhrPromise;
+export default xhr;
